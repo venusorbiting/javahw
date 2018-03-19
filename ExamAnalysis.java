800a3237d2fc289aa110b401888fa57b4205c598
Alicia Curtis <aliciahelen22@gmail.com>
2018-03-18 21:30:04 -0400

    aaaaaaaaaaahhhhhh

--- ExamAnalysis.java
+++ ExamAnalysis.java
@@ -2,7 +2,7 @@
     CS E-10b Spring 2018
     PS 5C
     Problem #6
-    DESCRIPTION OF THE PROGRAM ***
+    Program that grades student responses to an exam, taking input from the user to get the correct answers.
 */
 
 import java.util.*;
@@ -16,17 +16,16 @@ class ExamAnalysis
         
         System.out.println("I hope you are ready to begin...\n");
         System.out.print("Please type the correct answers to the exam questions, one right after the other: ");
-        //String correctAnswers = keyboard.nextLine();
-        String correctAnswers = "ABCEDBACED";            // REMOVE THIS, JUST FOR TESTING
+        String correctAnswers = keyboard.nextLine();
         int numOfQs = correctAnswers.length();
         
-        System.out.print("What is the name of the file containing each student's responses to the " + numOfQs + " questions? \n");
-        // File f = new File (keyboard.nextLine());
-        File f = new File ("exams.dat");                 // REMOVE THIS, JUST FOR TESTING
+        System.out.print("What is the name of the file containing each student's responses to the " + numOfQs + " questions? ");
+        File f = new File (keyboard.nextLine());
         Scanner inFile = new Scanner (f);
         // ADD A TRY/CATCH HERE        
         //if (!(f.exists() && f.canRead())) System.out.println("Sorry, that's not a valid file.");
         
+        // initialize variables
         int lineCount = 0;
         int correct = 0;
         int incorrect = 0;
@@ -41,8 +40,6 @@ class ExamAnalysis
             blank = 0;
             validInput = true;
             String thisAnswer = inFile.nextLine();
-            //correctQuestions.add(0);
-            //System.out.println("Test, the length of the Qs array is now " + correctQuestions.size());
             if (thisAnswer.length() == numOfQs)
             {
                 for (int i = 0; i < numOfQs; i++)
@@ -53,42 +50,27 @@ class ExamAnalysis
                         {
                             case 'A':
                                 fullAnswers[i*6]++;
-                                if (correctAnswers.charAt(i) == 'A')
-                                {
-                                    correct++;
-                                }
+                                if (correctAnswers.charAt(i) == 'A') correct++;
                                 else incorrect++;
                                 continue;
                             case 'B':
                                 fullAnswers[(i * 6)+1]++;
-                                if (correctAnswers.charAt(i) == 'B')
-                                {
-                                    correct++;
-                                }
+                                if (correctAnswers.charAt(i) == 'B') correct++;
                                 else incorrect++;
                                 continue;
                             case 'C':
                                 fullAnswers[(i * 6)+2]++;
-                                if (correctAnswers.charAt(i) == 'C')
-                                {
-                                    correct++;
-                                }
+                                if (correctAnswers.charAt(i) == 'C') correct++;
                                 else incorrect++;
                                 continue;
                             case 'D':
                                 fullAnswers[(i * 6)+3]++;
-                                if (correctAnswers.charAt(i) == 'D')
-                                {
-                                    correct++;
-                                }
+                                if (correctAnswers.charAt(i) == 'D') correct++;
                                 else incorrect++;
                                 continue;
                             case 'E':
                                 fullAnswers[(i * 6)+4]++;
-                                if (correctAnswers.charAt(i) == 'E')
-                                {
-                                    correct++;
-                                }
+                                if (correctAnswers.charAt(i) == 'E') correct++;
                                 else incorrect++;
                                 continue;
                             case ' ':
@@ -104,41 +86,63 @@ class ExamAnalysis
                 {
                     lineCount++;
                     System.out.println("Student #" + lineCount + "'s responses: " + thisAnswer);
-                    System.out.println(correct + " " + incorrect + " " + blank);
                     studentTally.addAll(Arrays.asList(correct, incorrect, blank));
-                    System.out.println("The student tally currently contains: " + studentTally);
                 }
             }
         }
         System.out.println("We have reached \"end of file!\"\n");
         System.out.println("Thank you for the data on " + lineCount + " students. Here's the analysis:\n");
         
-        for (int j = 0; j < numOfQs; j++)
+        System.out.println("Student #    Correct    Incorrect    Blank");
+        System.out.println("---------    -------    ---------    -----");
+        for (int k = 0; k < studentTally.size() / 3; k++)
         {
-            System.out.println("Question " + (j+1) + ": " + fullAnswers[j*6] + " " + fullAnswers[(j*6)+1] + " " + fullAnswers[(j*6)+2] + " " + 
-            fullAnswers[(j*6)+3] + " " + fullAnswers[(j*6)+4] + " " + fullAnswers[(j*6)+5]);
+            System.out.println("    " + (k + 1) + "           " + studentTally.get(k*3) + "          " + studentTally.get(k*3 + 1)
+            + "           " + studentTally.get(k*3 + 2));
         }
         
+        System.out.println("\n");
+        System.out.println("QUESTION ANALYSIS (* marks the correct response)");
+        System.out.println("-----------------\n");
         
-        /* THINGS THAT NEED TO GO HERE:
-        // Student   # Correct      Incorrect   Blank
-         ~~~~~~~~~     ~~~~~~~      ~~~~~~~~~   ~~~~~
-            1          7                2          1
-            2          8                1          1
-            etc
+        for (int j = 0; j < numOfQs; j++)
+        {
+            System.out.println("Question #" + (j+1) + ":");
+            switch (correctAnswers.charAt(j))
+            {
+                case 'A':
+                    System.out.println("A*      B       C       D       E       Blank");
+                    break;
+                case 'B':
+                    System.out.println("A       B*      C       D       E       Blank");
+                    break;
+                case 'C':
+                    System.out.println("A       B       C*      D       E       Blank");
+                    break;
+                case 'D':
+                    System.out.println("A       B       C       D*      E       Blank");
+                    break;
+                case 'E':
+                    System.out.println("A       B       C       D       E*      Blank");
+                    break;
+                default: System.out.println("Something went wrong.");
+            }
+            int ans1 = fullAnswers[(j*6)];
+            int ans2 = fullAnswers[(j*6)+1];
+            int ans3 = fullAnswers[(j*6)+2];
+            int ans4 = fullAnswers[(j*6)+3];
+            int ans5 = fullAnswers[(j*6)+4];
+            int ans6 = fullAnswers[(j*6)+5];
             
-        QUESTION ANALYSIS (* marks the correct response)
-        Question 1:
-        
-        A*      B       C       D       E       Blank
-        4       2       1       1       0       1
-        44.4%   22.2%   11.1%   11.1%   0.0%    11.1%
-        
-        Question 2:
-        A       B*      C       D       E       Blank
-        etc
-
-
-*/
+            System.out.println(fullAnswers[j*6] + "       " + fullAnswers[(j*6)+1] + "       " + fullAnswers[(j*6)+2] + "       " + 
+            fullAnswers[(j*6)+3] + "       " + fullAnswers[(j*6)+4] + "       " + fullAnswers[(j*6)+5]);
+            
+            inFile.close();
+            
+            // PERCENTAGE BULLSHIT TESTING AAAAAAGGGGGHH
+            System.out.println((fullAnswers[j*6] * (float)100) / numOfQs);
+            System.out.println(((float)fullAnswers[j*6] * (float)100) / (float)numOfQs);
+            System.out.println((fullAnswers[j*6] * 100.0f) / numOfQs);
+        }
     }
 }
